e828df72c46ee0d37884f23fff25dea15d7e2578
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-07-09 17:15:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -112,4 +112,15 @@ class TicTacToe
   def over?
     draw? || won?
   end
+
+  def winner
+    WIN_COMBINATIONS.detect do |win_combo|
+      if @board[win_combo[0]] == "X" && @board[win_combo[1]] == "X" && @board[win_combo[2]] == "X"
+        return "X"
+      elsif @board[win_combo[0]] == "O" && @board[win_combo[1]] == "O" && @board[win_combo[2]] == "O"
+        return "O"
+      else
+        false
+      end
+  end
 end
